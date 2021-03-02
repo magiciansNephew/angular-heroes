87bcf316b09f3a628c4443b03f63e0a7e66e4a36
Pramod <pramodlimbu@nepotech.com>
2021-03-02 18:56:07 +0545

    chaining RxJS operators

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';  
+import { catchError, tap } from 'rxjs/operators';  
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -53,6 +52,31 @@ export class HeroService {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
       tap( (newHero: Hero) => this.log(`added hero w/ id= ${newHero.id}`) ),
       catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /**DELETE: delete the hero from the server */
+  deleteHero(hero: Hero | number): Observable<Hero>{
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  /*GET heroes whose name containes search term */
+  searchHeroes(term: string): Observable<Hero[]>{
+    if(!term.trim()){
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(term => term.length ?
+            this.log(`found ${term.length} heroes matching "${term}"`):
+            this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
     );
   }
 
