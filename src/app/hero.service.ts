3fd99a8e41086de8ea02822c5c93d3abd8d6fdfe
Pramod <pramodlimbu@nepotech.com>
2021-03-02 09:24:03 +0545

    add hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes'; //URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type':'application/json' })
+  }
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
@@ -34,6 +38,22 @@ export class HeroService {
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     )
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any>{
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`update hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    )
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap( (newHero: Hero) => this.log(`added hero w/ id= ${newHero.id}`) ),
+      catchError(this.handleError<Hero>('addHero'))
+    );
   }
 
   private log(message: string) {
